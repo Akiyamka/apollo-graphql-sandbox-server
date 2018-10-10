a45b2e59b3b504f8c9a8328e9008acabff2f0b9d
akiyamka <akiyamka@gmail.com>
2018-10-10 14:39:47 +0300

    some cleaning code

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { libraryAPI, bankAPI } = require('./mocks');
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for. A more complete example might fetch
-// from an existing data source like a REST API or database.
-const mock = {
-  books: [
-    {
-      title: 'Harry Potter and the Chamber of Secrets',
-      author: 'J.K. Rowling',
-    },
-    {
-      title: 'Jurassic Park',
-      author: 'Michael Crichton',
-    },
-  ],
-  rates: [
-    {
-      currency: 'USD',
-      value: '0.46'
-    },
-    {
-      currency: 'BYN',
-      value: '1'
-    },
-    {
-      currency: 'RUB',
-      value: '76.24'
-    }
-  ]
-}
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
   type Book {
     title: String
     author: String
@@ -46,8 +12,6 @@ const typeDefs = gql`
     value: Float
   }
 
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
   type Query {
     books: [Book],
     rates: [Rate]
@@ -58,8 +22,8 @@ const typeDefs = gql`
 // schema. We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => mock.books,
-    rates: () => mock.rates
+    books: () => libraryAPI.books,
+    rates: () => bankAPI.rates
   },
 };
 
---
+++ mocks.js
@@ -0,0 +1,29 @@
+exports.libraryAPI = {
+  books: [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+    },
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton',
+    },
+  ]
+}
+
+exports.bankAPI = {
+  rates: [
+    {
+      currency: 'USD',
+      value: '0.46'
+    },
+    {
+      currency: 'BYN',
+      value: '1'
+    },
+    {
+      currency: 'RUB',
+      value: '76.24'
+    }
+  ]
+}
